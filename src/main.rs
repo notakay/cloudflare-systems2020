239a055c8ec379946a663b9aae31022d8e67c057
akay <aungkhantko@protonmail.com>
2020-10-19 00:43:45 +0630

    Got regex for URL

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
 use std::env;
 use std::net::*;
 use getopts::Options;
+use regex::Regex;
+
+fn request_constructor(url: &str) -> (String, String) {
+
+    let regex = Regex::new(r"(?i)^(https?)://([^\s$.?#/][a-zA-Z0-9_.]*)/*([^\s$?#]*)").unwrap();
+
+    if !regex.is_match(&url) {
+        panic!{"Invalid URL. URL must be in format http(s)://example.com/(resource)"};
+    }
+
+    let captures = regex.captures(&url).unwrap(); // Check if this fails??
+
+    let protocol = captures.get(1).map_or("", |m| m.as_str());
+    let host = captures.get(2).map_or("", |m| m.as_str());
+    let resource = captures.get(3).map_or("", |m| m.as_str());
+
+    let port = match &protocol.to_lowercase()[..] {
+        "http" => "80",
+        "https" => "443",
+        _ => {
+            panic!{"Invalid Protocol. Use HTTP or HTTPS"};
+        }
+    };
+
+    let mut url = String::from("");
+    url.push_str(host);
+    url.push_str(":");
+    url.push_str(port);
+
+    (url, resource.to_string())
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -26,8 +57,11 @@ fn main() {
         None => return
     };
 
-    // check for http or https
+    let (url, resource) = request_constructor(&url[..]);
 
-    //let mut addr_iter = "general-engineering-assignment.akay.workers.dev:443".to_socket_addrs().unwrap();
-    //println!("{:?}", addr_iter.next());
+    println!("{} {}", url, resource);
+
+    let addrs_iter = &mut url.to_socket_addrs().unwrap();
+    println!("{:?}", addrs_iter.next());
+
 }
