fe6a45127a284d31b375bd105d1e6c55a0294b90
akay <aungkhantko@protonmail.com>
2020-10-20 00:40:32 +0630

    Finished. To add some documentation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{env, process, thread};
 use std::io::{Write};
 use std::net::*;
 use std::sync::Arc;
+use std::collections::HashSet;
 
 use regex::Regex;
 use devtimer::DevTime;
@@ -21,6 +22,14 @@ fn main() {
     let host = Arc::new(host);
 
     let mut children = vec![];
+    let mut err_codes = HashSet::new();
+
+    let mut max_size = usize::MIN;
+    let mut min_size = usize::MAX;
+    let mut times = vec![];
+    let mut time_total:u128 = 0;
+    let mut success = 0;
+    let mut fail = 0;
 
     for _ in 0..count {
         let r_message = message.clone();
@@ -29,11 +38,6 @@ fn main() {
 
         children.push(thread::spawn(move || make_request(&r_message, &r_ip, &r_host, ssl, profile)));
     }
-
-    let mut max_size = usize::MIN;
-    let mut min_size = usize::MAX;
-    let mut times = vec![];
-    let mut time_total:u128 = 0;
 
    	for child in children {
         match child.join() {
@@ -47,9 +51,16 @@ fn main() {
                     min_size = x.0;
                 }
 
+                if x.2 == "200" {
+                    success += 1;
+                } else {
+                    err_codes.insert(x.2);
+                    fail += 1;
+                }
+
                 times.push(x.1);
                 time_total += x.1;
-            }
+            },
             Err(_) => {}
         };
     }
@@ -57,11 +68,13 @@ fn main() {
     if profile {
         times.sort();
 
-        println!("Max size: {} bytes, Min size: {} bytes", max_size, min_size);
+        let rate = (success as f32 / count as f32) * 100.0;
 
-        let num_requests = times.len() as u128;
-        println!("Max time: {} ms, Min time: {} ms, Mean time: {} ms, Median time: {} ms",
-                 times[times.len()-1], times[0], time_total/num_requests, times[times.len()/2]);
+        println!("Made {} GET requests with {}% success rate ({} requests failed)", count, rate, fail);
+        println!("The maximum response size was {} bytes. The minimum response size was {} bytes", max_size, min_size);
+        println!("The maximum time taken to receive the response was {} ms. The minimum time taken to receive the response was {} ms.", times[times.len()-1], times[0]);
+        println!("The average time to download the response was {} ms, whereas the median was {} ms.", time_total/count as u128, times[times.len()/2]);
+        println!("Error Codes: {:?}", err_codes);
     }
 }
 
@@ -136,10 +149,25 @@ fn delim_url(url: &str) -> (String, String, String, bool) {
 
 }
 
-fn read_stream<T: std::io::Read>(stream: &mut T, profile: bool) -> usize {
+fn extract_http_code(buffer: &str) -> String {
+    let regex = Regex::new(r"(?i)HTTP/.* (\d{3}) .*").unwrap();
+    let captures = match regex.captures(&buffer) {
+        Some(c) => c,
+        None => {
+            println!("Invalid HTTP Response");
+            process::exit(1);
+        }
+    };
+    captures.get(1).map_or("", |m| m.as_str()).to_string()
+}
+
+
+fn read_stream<T: std::io::Read>(stream: &mut T, profile: bool) -> (usize, String) {
 
     let mut buffer = [0u8; 1000];
     let mut bytes = 0;
+    let mut get_status = false;
+    let mut status_code = String::from("");
 
     loop {
         let bytes_read = stream.read(&mut buffer).unwrap();
@@ -147,6 +175,11 @@ fn read_stream<T: std::io::Read>(stream: &mut T, profile: bool) -> usize {
             bytes += bytes_read;
             if !profile {
                 print!("{}", String::from_utf8_lossy(&buffer));
+            }
+            if !get_status {
+                status_code = String::from_utf8_lossy(&buffer[0..15]).to_string();
+                status_code = extract_http_code(&status_code);
+                get_status = true;
             }
         } else {
             break;
@@ -157,30 +190,30 @@ fn read_stream<T: std::io::Read>(stream: &mut T, profile: bool) -> usize {
         println!("");
     }
 
-    bytes
+    (bytes, status_code)
 
 }
 
-fn make_request(message: &str, ip: &SocketAddr, host: &str, ssl: bool, profile: bool) -> (usize, u128) {
+fn make_request(message: &str, ip: &SocketAddr, host: &str, ssl: bool, profile: bool) -> (usize, u128, String) {
     let message = message.as_bytes();
 
     let mut timer = DevTime::new_simple();
     timer.start();
     let mut stream = TcpStream::connect(ip).unwrap();
-    let bytes;
+    let r_tuple;
 
     if ssl {
         let connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
         let mut ssl_stream = connector.connect(&host, stream).unwrap();
         ssl_stream.write_all(message).unwrap();
-        bytes = read_stream(&mut ssl_stream, profile);
+        r_tuple = read_stream(&mut ssl_stream, profile);
     } else {
         stream.write_all(message).unwrap();
-        bytes = read_stream(&mut stream, profile);
+        r_tuple = read_stream(&mut stream, profile);
     }
     timer.stop();
     let time = timer.time_in_millis().unwrap();
-    (bytes, time)
+    (r_tuple.0, time, r_tuple.1)
 }
 
 fn resolve_host(url: &str) -> SocketAddr {
@@ -240,7 +273,13 @@ fn parse_args(args: &[String]) -> (String, i32, bool) {
         Some(p) => {
             profile = true;
             match p.trim().parse() {
-                Ok(x) => x,
+                Ok(x) => {
+                    if x <= 0{
+                        println!("Argument for profile must be greater than 0");
+                        process::exit(1);
+                    }
+                    x
+                },
                 Err(_) => {
                     println!("Failed to argument for profile");
                     process::exit(1)
