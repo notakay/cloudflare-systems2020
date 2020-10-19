30015158898b76d14def2a057e84287e816581f0
akay <aungkhantko@protonmail.com>
2020-10-19 11:21:16 +0630

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ use std::net::*;
 use getopts::Options;
 use regex::Regex;
 use std::io::{Read, Write};
-use std::str::from_utf8;
 
 fn request_constructor(url: &str) -> (String, String, String) {
 
-    let regex = Regex::new(r"(?i)^(https?)://([^\s$.?#/][a-zA-Z0-9_\-.]*)/*([^\s$?#]*)").unwrap();
+    let regex = Regex::new(r"(?i)([a-z0-9._\-]*)/*([a-z0-9._\-/]*)").unwrap();
 
     let captures = match regex.captures(&url) {
         Some(c) => c,
@@ -18,18 +17,9 @@ fn request_constructor(url: &str) -> (String, String, String) {
         }
     };
 
-    let protocol = captures.get(1).map_or("", |m| m.as_str());
-    let host = captures.get(2).map_or("", |m| m.as_str());
-    let resource = captures.get(3).map_or("", |m| m.as_str());
-
-    let port = match &protocol.to_lowercase()[..] {
-        "http" => "80",
-        "https" => "443",
-        _ => {
-            println!("Invalid URL");
-            process::exit(0);
-        }
-    };
+    let host = captures.get(1).map_or("", |m| m.as_str());
+    let resource = captures.get(2).map_or("", |m| m.as_str());
+    let port = "80";
 
     let mut url = String::from("");
     url.push_str(host);
@@ -39,7 +29,49 @@ fn request_constructor(url: &str) -> (String, String, String) {
     (host.to_string(), url, resource.to_string())
 }
 
-fn parse_args(args: &[String]) -> (String, i32){
+fn make_request(host: &str, resource: &str, ip: SocketAddr) {
+
+    let mut stream = TcpStream::connect(ip).unwrap();
+
+    let mut message = String::from("GET /");
+    message.push_str(&resource);
+    message.push_str(" HTTP/1.1\r\n");
+    message.push_str("Host: ");
+    message.push_str(&host);
+    message.push_str("\r\n\r\n");
+
+    stream.write(message.as_bytes()).unwrap();
+    let mut buffer = [0; 1000 * 1000];
+
+    stream.read(&mut buffer).unwrap();
+    println!("{}", String::from_utf8_lossy(&buffer[..]));
+
+}
+
+fn resolve_host(url: String) -> SocketAddr {
+
+    let resolve = &mut url[..].to_socket_addrs();
+    let addrs_iter = match resolve {
+        Ok(i) => i,
+        Err(_) => {
+            println!("Could not resolve host.");
+            println!("Please make sure a valid URL is being used.");
+            println!("URL must be in format [www.example.com/resource].");
+            process::exit(0)
+        }
+    };
+
+    match addrs_iter.next() {
+        Some(i) => i,
+        None => {
+            println!("Could not resolve host");
+            process::exit(0)
+        }
+    }
+
+}
+
+fn parse_args(args: &[String]) -> (String, i32) {
 
     let mut opts = Options::new();
 
@@ -71,52 +103,14 @@ fn parse_args(args: &[String]) -> (String, i32){
     };
 
     (url, profile)
+
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let (url, profile) = parse_args(&args);
-    println!("{} {}", url, profile);
-
     let (host, url, resource) = request_constructor(&url[..]);
-    println!("{} {}", url, resource);
+    let ip = resolve_host(url);
 
-    let resolve = &mut url[..].to_socket_addrs();
-    let addrs_iter = match resolve {
-        Ok(i) => i,
-        Err(_) => {
-            println!("Could not resolve host");
-            process::exit(0)
-        }
-    };
-
-    let ip = match addrs_iter.next() {
-        Some(i) => i,
-        None => {
-            println!("Could not resolve host");
-            process::exit(0)
-        }
-    };
-
-    let mut stream = TcpStream::connect(ip).unwrap();
-
-    let mut message = String::from("");
-    message.push_str("GET / HTTP/1.1\r\n");
-    message.push_str("Host: ");
-    message.push_str(&host);
-    message.push_str("\r\n");
-    message.push_str("Connection: keep-alive");
-    message.push_str("\r\n\r\n");
-
-    println!("Message: {}", message);
-
-    stream.write(message.as_bytes()).unwrap();
-    let mut buffer = [0; 13000];
-
-    stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-
-    println!("{:?}", stream);
-
+    make_request(&host, &resource, ip);
 }
